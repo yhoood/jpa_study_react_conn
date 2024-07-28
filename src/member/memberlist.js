a61c7327de3121f5056735e03a4afcec627273b1
yhood <kjuws9940@gmail.com>
2024-07-28 20:42:16 +0900

    멤버정보 조회 기능 추가

--- src/member/memberlist.js
+++ src/member/memberlist.js
@@ -5,10 +5,10 @@ import { useEffect ,useState } from 'react';
 export function MemberList() {
   useEffect(()=>{
     axios.get('/member/memberList')
+    // axios.get('/member/memberList?id="abc"')
     .then((response)=>{
       console.log(response.data)
-      setList([response.data])
-      console.log(list)
+      setList(response.data)
     })
     .catch(()=>{
       console.log('실패함ㅅㄱ')
@@ -21,13 +21,25 @@ export function MemberList() {
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>이름</th>
+          <th>도시</th>
+          <th>거리</th>
+          <th>우편번호</th>
         </tr>
       </thead>
       <tbody>
+        {
+          list.map(function (a){
+            return(
+                    <tr>
+                      <td>{a.memberName}</td>
+                      <td>{a.address.city}</td>
+                      <td>{a.address.street}</td>
+                      <td>{a.address.zipcode}</td>
+                    </tr>
+                   )
+          })
+        }
         <tr>
           <td>1</td>
           <td>Mark</td>
